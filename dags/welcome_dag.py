7649ae2fadb2c9291f9233272cb14d05e741d72d
KyTranDE <21011801.ky@student.iuh.edu.vn>
2024-10-24 08:07:50 +0000

    Refactor welcome_dag.py and docker-compose.yaml, and move getData.py to dags directory

--- dags/welcome_dag.py
+++ dags/welcome_dag.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from getData
 
 # A DAG represents a workflow, a collection of tasks
 with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
