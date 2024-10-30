bb06d988ba0c0b9d56d13f195c406769fd4a3f17
KyTranDE <21011801.ky@student.iuh.edu.vn>
2024-10-30 15:03:51 +0000

    Refactor logging configuration in run_scraping function

--- sources/welcome_dag.py
+++ sources/welcome_dag.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from getData import run_scraping
+from sources.source.getData import run_scraping
 
 with DAG(
     dag_id="data_scraping_dag",
